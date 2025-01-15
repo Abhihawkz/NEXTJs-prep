7e2237532915e7cd7fdc42712a10008729859090
Abhishek <abhipro01@gmail.com>
2025-01-15 20:03:40 +0530

    added prisma and basic boilerplate

--- serveractions/app/api/auth/route.ts
+++ serveractions/app/api/auth/route.ts
@@ -1,4 +1,8 @@
+import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+
+
+const client = new PrismaClient()
 
 export function GET(){
     return NextResponse.json({username:"Healthcheck",email:"healthcheck"})
@@ -6,9 +10,17 @@ export function GET(){
 
 export async function POST(req:NextRequest){
     const body = await req.json()
+
+    const res = await client.user.create({data:{
+        username:body.username,
+        password:body.password
+    }})
+    
+ 
+ 
     console.log(body);
     console.log(req.headers.get("authorization"))
     console.log(req.nextUrl.searchParams.get("name"))
-    return NextResponse.json({body})
+    return NextResponse.json({res})
 
 }
