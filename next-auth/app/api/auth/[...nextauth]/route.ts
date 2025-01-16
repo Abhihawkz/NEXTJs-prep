318e936e925ce960bb2debcf7a35fa8c1cb11932
Abhishek <abhipro01@gmail.com>
2025-01-16 18:57:54 +0530

    next-auth client session

--- next-auth/app/api/auth/[...nextauth]/route.ts
+++ next-auth/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-
+import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
     providers:[
@@ -11,6 +11,8 @@ const handler = NextAuth({
               password: { label: "Password", type: "password",placeholder:"Enter your  password" }
             },
             async authorize(credentials, req) {
+              console.log(credentials)
+
               const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
         
               if (user) {
@@ -19,8 +21,13 @@ const handler = NextAuth({
                 return null        
               }
             }
+          }),
+          GoogleProvider({
+            clientId: "kdjaf",
+            clientSecret: "ahfaf"
           })
-    ]
+    ],
+    secret:process.env.NEXTAUTH_SECRET
 });
 
-export {handler as GET, handler as POST}
+export { handler as POST, handler as GET}
--- next-auth/app/page.tsx
+++ next-auth/app/page.tsx
@@ -1,7 +1,19 @@
-import Image from "next/image";
+"use client"
 
+import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 export default function Home() {
-  return (
-    <div>GG</div>
-  );
+
+  const serssion = useSession()
+  
+      return (
+      <div>{JSON.stringify(serssion)}
+          {serssion.status === "authenticated" && <button onClick={()=>{
+              signOut()
+          }}>Logout</button>}
+          {serssion.status === "unauthenticated" && <button onClick={()=>{
+              signIn()
+          }}>Signin</button>}
+      </div>
+    );
 }
